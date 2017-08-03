166c468e53f2f0c77bec68f0de4121c89bf40345
Robert Smith <robert@rigetti.com>
2017-08-03 15:31:18 -0700

    stylistic clean up

--- high-level.lisp
+++ high-level.lisp
@@ -6,32 +6,32 @@
   data)
 
 (defun make-complex-vector (&rest entries)
-  "Makes a complex vector out entries, a list of complex numbers."
-  (let ((v (fnv:make-fnv-complex-double (length entries)))
-        (i 0))
-    (over-fnv-complex-double (p) v
-      (let ((e (nth i entries)))
-        (assert (numberp e) () "~S is not a number" e)
-        (setf p (coerce e (list 'complex 'double-float)))
-        (incf i)))
-    (values v)))
+  "Makes a complex vector out ENTRIES, a list of complex numbers."
+  (let* ((len (length entries))
+         (v (fnv:make-fnv-complex-double len)))
+    (loop :for i :below len
+          :for e :in entries
+          :do (progn
+                (check-type e number)
+                (setf (fnv:fnv-complex-double-ref v i) (coerce e '(complex double-float))))
+          :finally (return v))))
 
 (defun complex-vector-to-list (v)
   "Make a list from a complex double fnv."
-  (let ((lst (list)))
-    (dotimes (i (fnv:fnv-length v))
-      (setf lst (append lst (list (fnv:fnv-complex-double-ref v i)))))
-    (values lst)))
+  (loop :for i :below (fnv:fnv-length v)
+        :collect (fnv:fnv-complex-double-ref v i)))
 
 (defun make-complex-matrix (m n &rest entries)
   "Makes an m-by-n matrix assuming entries is a list of complex numbers in column major order."
   (let ((entries-size (length entries))
         (expected-size (* m n)))
-    (assert (eq entries-size expected-size)
+    (assert (= entries-size expected-size)
             ()
-            "Length of entries is ~S, is not ~S*~S = ~S" 
+            "Length of entries is ~D, is not ~D * ~D = ~D" 
             entries-size m n expected-size)
-    (values (make-matrix :rows m :cols n :data (apply #'make-complex-vector entries)))))
+    (make-matrix :rows m
+                 :cols n
+                 :data (apply #'make-complex-vector entries))))
 
 (defun diag (m n &rest entries)
   "Creates a matrix with entries along the diagonal"
@@ -41,34 +41,41 @@
             "Min dimension is ~S but number of entries is ~S" expected-size entries-size)
     (let ((mat (apply #'make-complex-matrix 
                       (append (list m n) (make-list (* m n) :initial-element 0)))))
-      (dotimes (i entries-size)
-        (set-val mat i i (nth i entries)))
-      (values mat))))
+      (dotimes (i entries-size mat)
+        (setf (ref mat i i) (nth i entries))))))
 
 (defun ref (m i j)
   "Accessor method for the element in the i-th row and j-th column of m, assuming zero indexing."
   (let ((rows (matrix-rows m))
         (cols (matrix-cols m))
         (data (matrix-data m)))
-    (assert (integerp i) () "row index ~S is not an integer" i)
-    (assert (integerp j) () "column index ~S is not an integer" j)
-    (assert (< -1 i rows) () "row index ~S is out of range" i)
-    (assert (< -1 j cols) () "col index ~S is out of range" j)
-    ; currently a bit hacky to make a generic call to fnv-ref for a particular type
-    (funcall (intern (concatenate 'string (string (type-of data)) "-REF")) data (+ (* rows j) i))))
+    (check-type i integer)
+    (check-type j integer)
+    (assert (< -1 i rows) () "row index ~D is out of range" i)
+    (assert (< -1 j cols) () "col index ~D is out of range" j)
+    (let ((idx (+ (* rows j) i)))
+      (etypecase data
+        (fnv:fnv-float          (fnv:fnv-float-ref data idx))
+        (fnv:fnv-double         (fnv:fnv-double-ref data idx))
+        (fnv:fnv-complex-float  (fnv:fnv-complex-float-ref data idx))
+        (fnv:fnv-complex-double (fnv:fnv-complex-double-ref data idx))))))
 
-(defun set-val (m i j val)
+(defun (setf ref) (new-value m i j)
   "Set the value of m_ij to val."
   (let ((rows (matrix-rows m))
         (cols (matrix-cols m))
         (data (matrix-data m)))
-    (assert (integerp i) () "row index ~S is not an integer" i)
-    (assert (integerp j) () "column index ~S is not an integer" j)
+    (check-type i integer)
+    (check-type j integer)
+    (check-type new-value number)
     (assert (< -1 i rows) () "row index ~S is out of range" i)
     (assert (< -1 j cols) () "col index ~S is out of range" j)
-    (assert (numberp val) () "value ~S is not a number" val)
-    (setf (fnv:fnv-complex-double-ref data (+ (* rows j) i)) 
-          (coerce val (list 'complex 'double-float)))))
+    (let ((idx (+ (* rows j) i)))
+      (etypecase data
+        (fnv:fnv-float          (setf (fnv:fnv-float-ref data idx) (coerce new-value 'single-float)))
+        (fnv:fnv-double         (setf (fnv:fnv-double-ref data idx) (coerce new-value 'double-float)))
+        (fnv:fnv-complex-float  (setf (fnv:fnv-complex-float-ref data idx) (coerce new-value '(complex single-float))))
+        (fnv:fnv-complex-double (setf (fnv:fnv-complex-double-ref data idx) (coerce new-value '(complex double-float))))))))
 
 (defun print-matrix (m)
   "Print method for matrices."
@@ -86,16 +93,16 @@
         (m (matrix-rows ma))
         (n (matrix-cols mb))
         (k (matrix-cols ma))
-        (alpha (coerce 1 (list 'complex 'double-float)))
+        (alpha (coerce 1 '(complex double-float)))
         (a (fnv:copy-fnv-complex-double (matrix-data ma)))
         (b (fnv:copy-fnv-complex-double (matrix-data mb)))
-        (beta (coerce 0 (list 'complex 'double-float))))
+        (beta (coerce 0 '(complex double-float))))
     (let ((lda m)
           (ldb k)
           (ldc m)
           (c (fnv:make-fnv-complex-double (* m n))))
       (magicl.blas-cffi::%zgemm transa transb m n k alpha a lda b ldb beta c ldc)
-      (values (make-matrix :rows m :cols n :data c)))))
+      (make-matrix :rows m :cols n :data c))))
 
 (defun qr (m)
   "Finds the QR factorization of the matrix m."
@@ -121,24 +128,25 @@
             (assert (= (imagpart diag-elt) 0) 
                     () "Diagonal element R_~S~S=~S is not real" j j diag-elt)
             (setf diag-elt (realpart diag-elt))
-            (if (> 0 diag-elt)
+            (if (minusp diag-elt)
                 (dotimes (i rows)
                   (if (<= j i (1- cols))
-                      (set-val r j i (- (ref r j i))))
-                  (set-val q i j (- (ref q i j)))))))
+                      (setf (ref r j i) (- (ref r j i))))
+                  (setf (ref q i j) (- (ref q i j)))))))
         (values q r)))))
 
 (defun qr-helper-get-r (a n)
   "Get the matrix R from the upper triangular portion of a, where n is the number of columns"
   (let ((m (/ (fnv:fnv-length a) n))
         (r (fnv:make-fnv-complex-double (* n n))))
+    (check-type m integer)
     (dotimes (j n)
       (dotimes (i n)
-        (let ((entry (coerce #C (0 0) (list 'complex 'double-float))))
+        (let ((entry (coerce 0 '(complex double-float))))
           (if (>= j i)
               (setf entry (fnv:fnv-complex-double-ref a (+ (* m j) i))))
           (setf (fnv:fnv-complex-double-ref r (+ (* n j) i)) entry))))
-    (values (make-matrix :rows n :cols n :data r))))
+    (make-matrix :rows n :cols n :data r)))
 
 (defun qr-helper-get-q (a tau n)
   "Get the matrix Q as a product of reflectors, from results given by ZGEQRF."
@@ -154,10 +162,10 @@
       (setf work (fnv:make-fnv-complex-double (max 1 lwork)))
       ; run it again with optimal workspace size
       (magicl.lapack-cffi::%zungqr m n k a lda tau work lwork info)
-      (values (make-matrix :rows m :cols n :data a)))))
+      (make-matrix :rows m :cols n :data a))))
 
 (defun svd (m)
-  "Find the SVD of a matrix m."
+  "Find the SVD of a matrix M. Return (VALUES U SIGMA Vt) where M = U*SIGMA*Vt"
   (let ((jobu "A")
         (jobvt "A")
         (rows (matrix-rows m))
@@ -181,7 +189,7 @@
         ; run it again with optimal workspace size
         (magicl.lapack-cffi::%zgesvd jobu jobvt rows cols a lda s u ldu vt ldvt 
                                      work lwork rwork info)
-        (let ((smat (fnv:make-fnv-double (* rows cols) :initial-value 0d0)))
+        (let ((smat (fnv:make-fnv-double (* rows cols) :initial-value 0.0d0)))
           (dotimes (i (min rows cols))
             (setf (fnv:fnv-double-ref smat (+ (* rows i) i)) (fnv-double-ref s i)))          
           (values (make-matrix :rows rows :cols rows :data u)
