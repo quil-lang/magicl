7b0bfd2a3e77454d4cda37e5b3510aef1ffff02a
Robert Smith <robert@rigetti.com>
2017-09-22 23:20:59 -0700

    run all examples in tests

--- tests/high-level-tests.lisp
+++ tests/high-level-tests.lisp
@@ -1,6 +1,7 @@
 ;;;; tests/high-level-tests.lisp
 ;;;;
 ;;;; Author: Joseph Lin
+;;;;         Robert Smith
 
 (in-package #:magicl-tests)
 
@@ -9,3 +10,7 @@
   (let* ((x (magicl::make-complex-matrix 3 3 6 4 2 1 -2 8 1 5 7))
          (d (magicl::det x)))
     (is (= d -306.0d0))))
+
+(deftest test-examples ()
+  "Run all of the examples. Does not check for their correctness."
+  (is (magicl-examples:all-examples)))
