9063b697622ac60bd773ee21920b037a063f5b99
Robert Smith <robert@rigetti.com>
2017-09-23 16:10:36 -0700

    compensate for bug in recent FIASCO

--- tests/suite.lisp
+++ tests/suite.lisp
@@ -6,6 +6,9 @@
 
 (defun run-magicl-tests (&key (verbose nil) (headless nil))
   "Run all MAGICL tests. If VERBOSE is T, print out lots of test info. If HEADLESS is T, disable interactive debugging and quit on completion."
+  ;; Bug in Fiasco commit fe89c0e924c22c667cc11c6fc6e79419fc7c1a8b
+  (setf fiasco::*test-run-standard-output* (make-broadcast-stream
+                                            *standard-output*))
   (cond
     ((null headless)
      (run-package-tests :package ':magicl-tests
