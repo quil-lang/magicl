77b82972fc49d5fb8ecb77266fd298e11163fbbe
Cole Scott <cscott@rigetti.com>
2019-11-03 15:19:24 -0800

    Reduce oddity in abstract-tensor-tests and include dim=1 case

--- tests/abstract-tensor-tests.lisp
+++ tests/abstract-tensor-tests.lisp
@@ -6,15 +6,15 @@
 
 (deftest test-tensor-shape ()
   "Test that the shape is returned correctly for tensors of dimension 1 to 8"
-  (let ((dimensions (make-list 1 :initial-element 1)))
-    (loop :for rank :from 2 :to 8
+  (let ((dimensions (list 1)))
+    (loop :for rank :from 1 :to 8
           :do (rplacd (last dimensions) (list rank))
               (is (equal dimensions (magicl:shape (magicl:empty dimensions)))))))
 
 (deftest test-tensor-rank ()
   "Test that the rank is returned correctly for tensors of dimension 1 to 8"
-  (let ((dimensions (make-list 1 :initial-element 1)))
-    (loop :for rank :from 2 :to 8
+  (let ((dimensions (list 1)))
+    (loop :for rank :from 1 :to 8
           :do (rplacd (last dimensions) (list rank))
               (is (equal rank (magicl:rank (magicl:empty dimensions)))))))
 
