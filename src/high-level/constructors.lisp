ec93aa7af627f5423c508bbe1af0523c4979eabc
Cole Scott <cscott@rigetti.com>
2019-11-03 15:15:14 -0800

    Extract function for type inferring in tensor constructors

--- src/high-level/constructors.lisp
+++ src/high-level/constructors.lisp
@@ -6,6 +6,11 @@
 
 (defconstant +default-tensor-type+ 'double-float)
 
+(defun infer-tensor-type (type default)
+  (if (null type)
+        (compatible-tensor-constructors-from-value default)
+        (values (compatible-tensor-constructors type) type)))
+
 (defgeneric empty (shape &key type order)
   (:documentation "Create an empty tensor of specified shape")
   (:method (shape &key (type +default-tensor-type+) order)
@@ -17,71 +22,52 @@
   (:documentation "Create tensor with all elements equal to a constant value")
   (:method (const shape &key type order)
     (check-type shape shape)
-    (let ((tensor-class
-            (if (null type)
-                (compatible-tensor-constructors-from-value const)
-                (compatible-tensor-constructors type)))
-          (element-type
-            (if (null type)
-                (upgraded-array-element-type (type-of const))
-                type)))
+    (multiple-value-bind (tensor-class element-type)
+        (infer-tensor-type type const)
       (specialize-tensor (make-tensor shape tensor-class element-type :order order :initial-element (coerce const element-type))))))
 
 (defgeneric rand (shape &key type distribution)
-  (:documentation "Create tensor with elements random in the range [0,limit]")
+  (:documentation "Create tensor with random elements")
   (:method (shape &key (type +default-tensor-type+) distribution)
-    (progn
-      (check-type shape shape)
-      (let* ((tensor-class (compatible-tensor-constructors type))
-             (rand-function
-               (or distribution
-                   (cond
-                     ((subtypep type 'complex)
-                      (lambda ()
-                        (complex
-                         (random 1d0)
-                         (random 1d0))))
-                     (t
-                      (lambda ()
-                        (random 1d0))))))
-             (f (lambda (&rest rest)
-                  (declare (ignore rest))
-                  (coerce  (funcall rand-function) type))))
-        (specialize-tensor (into! f (make-tensor shape tensor-class type)))))))
+    (check-type shape shape)
+    (let* ((tensor-class (compatible-tensor-constructors type))
+           (rand-function
+             (or distribution
+                 (cond
+                   ((subtypep type 'complex)
+                    (lambda ()
+                      (complex
+                       (random 1d0)
+                       (random 1d0))))
+                   (t
+                    (lambda ()
+                      (random 1d0))))))
+           (f (lambda (&rest rest)
+                (declare (ignore rest))
+                (coerce  (funcall rand-function) type))))
+      (specialize-tensor (into! f (make-tensor shape tensor-class type))))))
 
 (defgeneric deye (d shape &key type order)
   (:documentation "Create identity matrix scaled by factor D")
   (:method (d shape &key type order)
     (check-type shape shape)
     (assert-square-shape shape)
-    (let* ((tensor-class
-             (if (null type)
-                 (compatible-tensor-constructors-from-value d)
-                 (compatible-tensor-constructors type)))
-           (element-type
-             (if (null type)
-                 (upgraded-array-element-type (type-of d))
-                 type))
-           (tensor (make-tensor shape tensor-class element-type :order order)))
-      (loop :for i :below (first shape)
-            :do (setf (tref tensor i i) (coerce d element-type)))
-      (specialize-tensor tensor))))
+    (multiple-value-bind (tensor-class element-type)
+        (infer-tensor-type type d)
+      (let ((tensor (make-tensor shape tensor-class element-type :order order)))
+        (loop :for i :below (first shape)
+              :do (setf (tref tensor i i) (coerce d element-type)))
+        (specialize-tensor tensor)))))
 
 (defgeneric arange (range &key type order)
   (:documentation "Create a 1d tensor of integers (not the type) from 0 up to but not including the specified range")
   (:method (range &key type order)
-    (let* ((tensor-class
-             (if (null type)
-                 (compatible-tensor-constructors-from-value range)
-                 (compatible-tensor-constructors type)))
-           (element-type
-             (if (null type)
-                 (upgraded-array-element-type (type-of range))
-                 type))
-           (tensor (make-tensor (list (floor range)) tensor-class element-type :order order))
-           (f (lambda (index)
-                (coerce index element-type))))
-      (specialize-tensor (into! f tensor)))))
+    (multiple-value-bind (tensor-class element-type)
+        (infer-tensor-type type range)
+      (let ((tensor (make-tensor (list (floor range)) tensor-class element-type :order order))
+            (f (lambda (index)
+                 (coerce index element-type))))
+        (specialize-tensor (into! f tensor))))))
 
 (defgeneric from-array (array shape &key type order)
   (:documentation "Create a tensor from an array")
@@ -90,10 +76,7 @@
              (if (null type)
                  (array-element-type array)
                  type))
-           (tensor-class
-             (if (null type)
-                 (compatible-tensor-constructors element-type)
-                 (compatible-tensor-constructors type))))
+           (tensor-class (compatible-tensor-constructors element-type)))
       (adjust-array array (list (reduce #'* shape)) :element-type element-type)
       (specialize-tensor
        (make-tensor shape tensor-class element-type
@@ -109,25 +92,19 @@ NOTE: When type is not specified, the type is inferred from the first element of
           (list-size (length list)))
       (assert (cl:= list-size shape-size)
               () "Incompatible shape. Must have the same total number of elements. The list has ~a elements and the new shape has ~a elements" list-size shape-size))
-    (let* ((tensor-class
-             (if (null type)
-                 (compatible-tensor-constructors-from-value (first list))
-                 (compatible-tensor-constructors type)))
-           (element-type
-             (if (null type)
-                 (upgraded-array-element-type (type-of (first list)))
-                 type))
-           (tensor (make-tensor shape tensor-class element-type :order order)))
-      (specialize-tensor
-       (into!
-        (lambda (&rest pos)
-          (coerce (nth
-                   (if (eql input-order :row-major)
-                       (row-major-index pos shape)
-                       (column-major-index pos shape))
-                   list)
-                  element-type))
-        tensor)))))
+    (multiple-value-bind (tensor-class element-type)
+        (infer-tensor-type type (first list))
+      (let ((tensor (make-tensor shape tensor-class element-type :order order)))
+        (specialize-tensor
+         (into!
+          (lambda (&rest pos)
+            (coerce (nth
+                     (if (eql input-order :row-major)
+                         (row-major-index pos shape)
+                         (column-major-index pos shape))
+                     list)
+                    element-type))
+          tensor))))))
 
 (defgeneric from-diag (list shape &key type order)
   (:documentation "Create a tensor of the specified shape from a list, placing along the diagonal
@@ -141,15 +118,9 @@ NOTE: When type is not specified, the type is inferred from the first element of
           (list-size (length list)))
       (assert (cl:= (* list-size list-size) shape-size)
               () "Incompatible shape. Must have the same total number of elements. The list has ~a diagonal elements and the new shape has ~a elements" list-size shape-size))
-    (let* ((tensor-class
-             (if (null type)
-                 (compatible-tensor-constructors-from-value (first list))
-                 (compatible-tensor-constructors type)))
-           (element-type
-             (if (null type)
-                 (upgraded-array-element-type (type-of (first list)))
-                 type))
-           (tensor (make-tensor shape tensor-class element-type :order order)))
-      (loop :for i :below (first shape)
-            :do (setf (tref tensor i i) (coerce (pop list) element-type)))
-      (specialize-tensor tensor))))
+    (multiple-value-bind (tensor-class element-type)
+        (infer-tensor-type type (first list))
+      (let ((tensor (make-tensor shape tensor-class element-type :order order)))
+        (loop :for i :below (first shape)
+              :do (setf (tref tensor i i) (coerce (pop list) element-type)))
+        (specialize-tensor tensor)))))
--- src/high-level/specialize-constructor.lisp
+++ src/high-level/specialize-constructor.lisp
@@ -16,12 +16,14 @@
       (t (error "No compatible tensor constructor for type ~a" type)))))
 
 ;; Maybe a bunch of :after to check nil -> do own check (like node does)
+;;
+;; This could possibly be changed to just return the type to use with COMPATIBLE-TENSOR-CONSTRUCTORS
 (defgeneric compatible-tensor-constructors-from-value (value)
-  (:documentation "Get compatible tensor class from element value")
+  (:documentation "Get compatible tensor class and type from element value")
   (:method (value)
     (etypecase value
-      (single-float 'tensor/single-float)
-      (double-float 'tensor/double-float)
-      ((complex single-float) 'tensor/complex-single-float)
-      ((complex double-float) 'tensor/complex-double-float)
-      ((signed-byte 32) 'tensor/int32))))
+      (single-float (values 'tensor/single-float 'single-float))
+      (double-float (values 'tensor/double-float 'double-float))
+      ((complex single-float) (values 'tensor/complex-single-float '(complex single-float)))
+      ((complex double-float) (values 'tensor/complex-double-float '(complex double-float)))
+      ((signed-byte 32) (values 'tensor/int32 '(signed-byte 32))))))
