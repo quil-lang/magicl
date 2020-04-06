71a1304f2ae03334e49cc1d13f80da02c33ee641
Cole Scott <me@colescott.io>
2020-04-05 17:12:12 -0700

    Implement offset in EYE and FROM-DIAG constructors (#101)
    * Implement offset in EYE constructor for matrices
    
    * Add offset in FROM-DIAG
    
    * Add notes on OFFSET in docstrings for EYE and FROM-DIAG
    
    * Allow for reassigning LAYOUT and ORDER in EYE and FROM-DIAG respectively
    
    Delay creation of tensors in EYE and FROM-DIAG to until after the assert to allow reassignment in signal
    Add another test in IDENTITY-MATRIX-P for offset EYE
    Change stray LOOPs to DOTIMES

--- src/high-level/constructors.lisp
+++ src/high-level/constructors.lisp
@@ -64,20 +64,36 @@ SHAPE can either be a list of dimensions or a fixnum defining the length of the 
 If VALUE is not specified then 1 is used.
 If TYPE is not specified then it is inferred from the type of VALUE, defaulting to *DEFAULT-TENSOR-TYPE*.
 LAYOUT specifies the internal storage representation ordering of the returned tensor.
-The tensor is specialized on SHAPE and TYPE."
-  (policy-cond:with-expectations (> speed safety)
-      ((type (or shape fixnum) shape)
-       (type fixnum offset))
-    (let* ((shape (if (integerp shape)
-                      (fixnum-to-shape shape)
-                      shape))
-           (tensor-class (infer-tensor-type (if value nil type) shape value))
-           (tensor (make-tensor tensor-class shape :layout layout))
-           (shape-length (length shape))
-           (fill-value (coerce (or value 1) (element-type tensor))))
-      (loop :for i :below (reduce #'min shape)
-            :do (setf (apply #'tref tensor (make-list shape-length :initial-element i)) fill-value))
-      tensor)))
+The tensor is specialized on SHAPE and TYPE.
+If OFFSET is specified then the diagonal band will be offset by that much, positive shifting up and negative shifting down."
+  (declare (type (or shape fixnum) shape)
+           (type fixnum offset))
+  (let ((shape (if (integerp shape)
+                   (fixnum-to-shape shape)
+                   shape)))
+    (cond
+      ((/= offset 0)
+       (assert (cl:= (length shape) 2)
+               (shape)
+               "The length of SHAPE must be 2 when OFFSET is specified. Shape has length ~A." (length shape))
+       ;; Now we know we are dealing with a matrix and can use offsets
+       ;;
+       ;; NOTE: We infer the tensor type this late to allow for
+       ;; reassignment of SHAPE in the assertion.
+       (let* ((tensor-class (infer-tensor-type (if value nil type) shape value))
+              (tensor (make-tensor tensor-class shape :layout layout))
+              (fill-value (coerce (or value 1) (element-type tensor)))) ;; TODO: use registry)
+         (loop :for i :from (max 0 (- offset)) :below (first shape)
+               :for j :from (max 0 offset) :below (second shape) :do
+                 (setf (tref tensor i j) fill-value))
+         tensor))
+      (t
+       (let* ((tensor-class (infer-tensor-type (if value nil type) shape value))
+              (tensor (make-tensor tensor-class shape :layout layout))
+              (fill-value (coerce (or value 1) (element-type tensor))) ;; TODO: use registry
+              (shape-length (length shape)))
+         (dotimes (i (reduce #'min shape) tensor)
+           (setf (apply #'tref tensor (make-list shape-length :initial-element i)) fill-value)))))))
 
 (defun arange (range &key (type *default-tensor-type*))
   "Create a 1-dimensional tensor of elements from 0 up to but not including the RANGE
@@ -139,21 +155,36 @@ The tensor is specialized on SHAPE and TYPE."
           (setf (apply #'tref tensor (funcall index-function i shape))
                 (pop list)))))))
 
-(defun from-diag (list &key (order 2) type layout)
+(defun from-diag (list &key (order 2) (offset 0) type layout)
   "Create a tensor from a list, placing along the diagonal
 
 If ORDER is specified then the tensor will be of that order, otherwise 2 is assumed.
 If TYPE is not specified then it is inferred from the type of the first element of LIST.
 LAYOUT specifies the internal storage representation ordering of the returned tensor.
-The tensor is specialized on SHAPE and TYPE."
-  (let* ((length (length list))
-         (shape (fixnum-to-shape length order))
-         (tensor-class (infer-tensor-type type shape (first list)))
-         (tensor (make-tensor tensor-class shape :layout layout)))
-    (loop :for i :below length
-          :do (setf (apply #'tref tensor (make-list order :initial-element i))
-                    (pop list)))
-    tensor))
+The tensor is specialized on SHAPE and TYPE.
+If OFFSET is specified then the diagonal band will be offset by that much, positive shifting up and negative shifting down."
+  (cond
+    ((/= offset 0)
+     (assert (cl:= order 2)
+             (order)
+             "ORDER must be 2 when OFFSET is specified. ORDER is ~A." order)
+     ;; Now we know we are dealing with a matrix.
+     (let* ((length (+ (length list) (abs offset)))
+            (shape (fixnum-to-shape length order))
+            (tensor-class (infer-tensor-type type shape (first list)))
+            (tensor (make-tensor tensor-class shape :layout layout)))
+       (loop :for i :from (max 0 (- offset)) :below (first shape)
+             :for j :from (max 0 offset) :below (second shape) :do
+               (setf (tref tensor i j) (pop list)))
+       tensor))
+    (t
+     (let* ((length (+ (length list) (abs offset)))
+            (shape (fixnum-to-shape length order))
+            (tensor-class (infer-tensor-type type shape (first list)))
+            (tensor (make-tensor tensor-class shape :layout layout)))
+       (dotimes (i (reduce #'min shape) tensor)
+         (setf (apply #'tref tensor (make-list order :initial-element i))
+               (pop list)))))))
 
 ;;; Constructors for convenience
 
