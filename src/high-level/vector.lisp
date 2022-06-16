7b3d9c894052077f3b9ec04c68b24f1f37d8837f
Robert Smith <rssmith@hrl.com>
2022-06-16 12:33:32 -0700

    move row/col matrix operations

--- src/high-level/vector.lisp
+++ src/high-level/vector.lisp
@@ -195,6 +195,7 @@ Special values of P are: :INFINITY :INF :POSITIVE-INFINITY"
   "Normalize the vector VECTOR in-place."
   (scale! vector (/ (norm vector p))))
 
+;;; These are implemented later in matrix-functions/row-column-matrices.lisp
 (defgeneric vector->row-matrix (vector)
   (:documentation "Convert a VECTOR to a row vector as a matrix. The output will share memory with the input."))
 (defgeneric vector->column-matrix (vector)
@@ -204,27 +205,7 @@ Special values of P are: :INFINITY :INF :POSITIVE-INFINITY"
 (defgeneric column-matrix->vector (matrix)
   (:documentation "Convert a MATRIX (a column vector) into a vector. The output will share memory with the input."))
 
-(macrolet ((define-vector-matrix-conversion (type)
-             (let* ((vec-type (alexandria:symbolicate "VECTOR/" type))
-                    (mat-type (alexandria:symbolicate "MATRIX/" type))
-                    (make-vec (alexandria:symbolicate "MAKE-" vec-type))
-                    (make-mat (alexandria:symbolicate "MAKE-" mat-type)))
-               `(progn
-                  (defmethod vector->row-matrix ((vector ,vec-type))
-                    (,make-mat 1 (size vector) (size vector) ':column-major (storage vector)))
-                  (defmethod vector->column-matrix ((vector ,vec-type))
-                    (,make-mat (size vector) 1 (size vector) ':column-major (storage vector)))
-                  (defmethod row-matrix->vector ((matrix ,mat-type))
-                    (assert (= 1 (nrows matrix)))
-                    (,make-vec (ncols matrix) (storage matrix)))
-                  (defmethod column-matrix->vector ((matrix ,mat-type))
-                    (assert (= 1 (ncols matrix)))
-                    (,make-vec (nrows matrix) (storage matrix)))))))
-  (define-vector-matrix-conversion single-float)
-  (define-vector-matrix-conversion double-float)
-  (define-vector-matrix-conversion complex-single-float)
-  (define-vector-matrix-conversion complex-double-float)
-  (define-vector-matrix-conversion int32))
+
 
 
 (defmethod map!-lisp (function (tensor vector))
