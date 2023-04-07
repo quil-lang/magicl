96097acd4bf809beb5ea56207b4eb744013a12c1
digikar99 <shubhamkarayare@gmail.com>
2023-04-07 19:08:49 +0530

    Add TARGET argument for MAP (https://github.com/quil-lang/magicl/issues/175)

--- src/high-level/abstract-tensor.lisp
+++ src/high-level/abstract-tensor.lisp
@@ -106,10 +106,10 @@ In the event TARGET is not specified, the result may return an array sharing mem
        (lambda (&rest dims)
          (apply #'(setf tref) (funcall function (apply #'tref source dims)) target dims))))))
 
-(defgeneric map (function tensor)
-  (:documentation "Map elements of TENSOR, storing the output of FUNCTION on the element into the corresponding element of a new tensor")
-  (:method ((function function) (tensor abstract-tensor))
-    (map! function (deep-copy-tensor tensor))))
+(defgeneric map (function tensor &optional target)
+  (:documentation "Map elements of TENSOR, storing the output of FUNCTION on the element into the corresponding element of TARGET if it is supplied or else a new tensor")
+  (:method ((function function) (tensor abstract-tensor &optional (target nil targetp)))
+    (map! function (if targetp target (deep-copy-tensor tensor)))))
 
 (defgeneric into (function tensor)
   (:documentation "Map indices of TENSOR, storing the output of FUNCTION on the index into the corresponding element of a new tensor
